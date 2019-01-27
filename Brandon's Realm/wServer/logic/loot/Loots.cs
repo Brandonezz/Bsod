9ef372fe26fd6ab059fe81cc7c7b56599e18f8ce
Chick3nBob <thagum21@gmail.com>
2019-01-27 15:33:27 -0600

    Fixed

--- Brandon's Realm/wServer/logic/loot/Loots.cs
+++ Brandon's Realm/wServer/logic/loot/Loots.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using wServer.networking.svrPackets;
 using wServer.realm;
 using wServer.realm.entities;
 using wServer.realm.entities.player;
